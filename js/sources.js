40ac42bb255f0aad5f191cd702fe3a7082b2c34b
jadedlebi <80483580+jadedlebi@users.noreply.github.com>
2024-03-07 16:07:03 -0500

    Create sources.js

---
+++ js/sources.js
@@ -0,0 +1,94 @@
+function addMapSources(map) {
+    map.addSource("hld-0", {
+        type: "vector",
+        url: "mapbox://jedlebi.ccbsa2pp"
+    });
+    map.addSource("hld-1", {
+        type: "vector",
+        url: "mapbox://jedlebi.4j2sv2my"
+    });
+    map.addSource("hld-2", {
+        type: "vector",
+        url: "mapbox://jedlebi.5x2r5vhi"
+    });
+    map.addSource("hld-3", {
+        type: "vector",
+        url: "mapbox://jedlebi.4ouwmgkt"
+    });
+    map.addSource("hld-4", {
+        type: "vector",
+        url: "mapbox://jedlebi.ac5vopai"
+    });
+    map.addSource("holc", {
+        type: "vector",
+        url: "mapbox://jedlebi.8mavlfd0"
+    });
+    map.addSource("holc-boundary", {
+        type: "vector",
+        url: "mapbox://jedlebi.7leow2ug"
+    });
+    map.addSource("hld-co", {
+        type: "vector",
+        url: "mapbox://jedlebi.bmvrpdb4"
+    });
+    map.addSource("hld-cbsa", {
+        type: "vector",
+        url: "mapbox://jedlebi.01w5rk3d"
+    });
+    map.addSource("info0", {
+        type: "vector",
+        url: "mapbox://jedlebi.ajhrnhgt"
+    });
+    map.addSource("info1", {
+        type: "vector",
+        url: "mapbox://jedlebi.2x0woyjb"
+    });
+    map.addSource("info2", {
+        type: "vector",
+        url: "mapbox://jedlebi.chk02pb3"
+    });
+    map.addSource("info31", {
+        type: "vector",
+        url: "mapbox://jedlebi.1pcy5o5g"
+    });
+    map.addSource("info32", {
+        type: "vector",
+        url: "mapbox://jedlebi.13hug3fz"
+    });
+    map.addSource("info33", {
+        type: "vector",
+        url: "mapbox://jedlebi.3km066cn"
+    });
+    map.addSource("info4", {
+        type: "vector",
+        url: "mapbox://jedlebi.chifdmkq"
+    });  
+    map.addSource("hld-outline-0", {
+        type: "vector",
+        url: "mapbox://jedlebi.031ncygs"
+    });
+    map.addSource("hld-outline-1", {
+        type: "vector",
+        url: "mapbox://jedlebi.cz5wmkeg"
+    });
+    map.addSource("hld-outline-2", {
+        type: "vector",
+        url: "mapbox://jedlebi.7dcwry8k"
+    });
+    map.addSource("hld-outline-31", {
+        type: "vector",
+        url: "mapbox://jedlebi.2tcmtusf"
+    });
+    map.addSource("hld-outline-32", {
+        type: "vector",
+        url: "mapbox://jedlebi.bfli84vg"
+    });
+    map.addSource("hld-outline-33", {
+        type: "vector",
+        url: "mapbox://jedlebi.9sq06n3r"
+    });
+    map.addSource("hld-outline-4", {
+        type: "vector",
+        url: "mapbox://jedlebi.40ll233f"
+    });
+}
